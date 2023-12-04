b1e9e82310f9debf8e80f2d3a86564b2e31cfb1c
Borbak820 <erikhaubrich@gmail.com>
2023-12-04 16:20:39 +0100

    Implemented SendID-extraction

--- QAMDecGen/qamdec.c
+++ QAMDecGen/qamdec.c
@@ -53,8 +53,6 @@
 #define onethreequartersjump1 55
 #define onethreequartersjump2 56 //Perfekt für Sync Weil diese Sprünge nur in einem Fall auftreten können 0 -> 3
 #define onethreequartersjump3 57
-/*Test Init*/
-
 
 /*Queue Init*/
 QueueHandle_t decoderQueue;
@@ -77,6 +75,8 @@ uint8_t debug = 0;
 uint8_t symbol = 0;
 uint8_t checksumGL = 0; // Initialisierung der Checksumme
 uint8_t calculatedChecksum = 0; // Variable für die berechnete Checksumme
+unsigned char SendIDArray[4];
+uint8_t SendID;
 unsigned char lenghtArray[4];
 uint8_t datalenght;
 unsigned char byteArray[4];
@@ -239,36 +239,30 @@ void onethreequartersjump(void){
 
 void analyzediff();
 
-void vQuamDec(void* pvParameters)
-{
+void vQuamDec(void* pvParameters){
 	( void ) pvParameters;
-
-	decoderQueue = xQueueCreate( 4, NR_OF_SAMPLES * sizeof(int16_t) );
 	
+	decoderQueue = xQueueCreate( 4, NR_OF_SAMPLES * sizeof(int16_t) );
 	while(evDMAState == NULL) {
 		vTaskDelay(3/portTICK_RATE_MS);
 	}
 	
 	uint16_t bufferelement[NR_OF_SAMPLES];
-	
 	xEventGroupWaitBits(evDMAState, DMADECREADY, false, true, portMAX_DELAY);
 	for(;;) {
 		while(uxQueueMessagesWaiting(decoderQueue) > 0) { // Nur arbeiten wenn in der Queue auch Werte drin sind
 			if(xQueueReceive(decoderQueue, &bufferelement[0], portMAX_DELAY) == pdTRUE) {
-				for (int i = 0; i < 32; i++) // Die Werte von der Queue werden in das Ringbuffer geschrieben
-				{
+				for (int i = 0; i < 32; i++) {// Die Werte von der Queue werden in das Ringbuffer geschrieben
 					*p_Writing = bufferelement[i];
 					p_Writing++;
 					Ringbuffer_Pos++;
 					//Differenz Wert
 					
 				}
-				if (((p_Writing - p_Reading)%32) == 0) //Überprüfen öb == sinnvoll ist eher >=. Mehr als 32 Wert vorraus
-				{			//Letztes Byte als Zähl Variabel nutzen
-					for (int i = 0; i < 32; i++)
-					{
-						if ((*p_Reading > *(p_Reading-1)) && *p_Reading > 1300) //Werte müssen angepasst werden mit den Werten von Merlin
-						{
+				if (((p_Writing - p_Reading)%32) == 0) { //Überprüfen öb == sinnvoll ist eher >=. Mehr als 32 Wert vorraus
+					//Letztes Byte als Zähl Variabel nutzen
+					for (int i = 0; i < 32; i++) {
+						if ((*p_Reading > *(p_Reading-1)) && *p_Reading > 1300) {//Werte müssen angepasst werden mit den Werten von Merlin
 							max = *p_Reading; //Bei der 3.ten Nachricht will das Programm hier nicht mehr reingehen weil die Pointeradressen nicht mehr sauber durch 32 teilbar sind
 							*p_MAXPOS2r = j;
 						}
@@ -279,8 +273,7 @@ void vQuamDec(void* pvParameters)
 					*p_MAXPOS1r = *p_MAXPOS2r;
 					p_Reading = p_Writing; //Sinnvoll? Decodieren vlt Separieren von schreiben in den Ringbuffer | vlt Neue Task erstellen?
 				}
-				if(Ringbuffer_Pos == 256) //% Operator | Bits maskieren!? Ringbuffer 0/256 == 0
-				{
+				if(Ringbuffer_Pos == 256) {//% Operator | Bits maskieren!? Ringbuffer 0/256 == 0
 					Ringbuffer_Pos = 0;
 					j = 0;
 					p_Writing = &ringbuffer[0]; //Der Code wurde Seriallisiert. Die Tasks
@@ -297,6 +290,37 @@ void vQuamDec(void* pvParameters)
 			} //Klammer While
 		} //Klammer For
 		vTaskDelay( 2 / portTICK_RATE_MS );
+	}
+}
+
+
+void getDataSendID(void) {
+	static int pReceivebuffer = 4;
+	static int SendIDArrayIndex = 0;
+	memcpy(&SendIDArray, 0, sizeof(uint8_t)); //Lösche inhalt vom Array
+
+	for (int i = 0; i < 4; i++) {
+		switch (receivebuffer[pReceivebuffer]) {
+			case 0:
+			SendIDArray[SendIDArrayIndex] |= (0b000 << (i % 4) * 2);
+			break;
+			case 1:
+			SendIDArray[SendIDArrayIndex] |= (0b001 << (i % 4) * 2);
+			break;
+			case 2:
+			SendIDArray[SendIDArrayIndex] |= (0b010 << (i % 4) * 2);
+			break;
+			case 3:
+			SendIDArray[SendIDArrayIndex] |= (0b100 << (i % 4) * 2);
+			break;
+		}
+		pReceivebuffer++;
+	}
+	memcpy(&SendID, SendIDArray, sizeof(uint8_t));
+	// Zurücksetzen von pReceivebuffer und byteArrayIndex, wenn es am Ende des receivebuffer angelangt ist
+	if (pReceivebuffer >=7) {
+		pReceivebuffer = 4;
+		SendIDArrayIndex = 0;
 	}
 }
 
@@ -322,9 +346,7 @@ void getDatalength(void) {
         }
         pReceivebuffer++;
     }
-
     memcpy(&datalenght, lenghtArray, sizeof(uint8_t));
-
     // Zurücksetzen von pReceivebuffer und byteArrayIndex, wenn es am Ende des receivebuffer angelangt ist
     if (pReceivebuffer >=11) {
         pReceivebuffer = 8;
@@ -441,8 +463,9 @@ void analyzediff(void){
 		break;
 	}
 	k++;
-	switch(k){
+	switch(k) {
 		case 32: //Wert noch anpassen working 62
+		getDataSendID();
 		getDatalength();
 		getDataTemp();
 		k = 0;
@@ -450,10 +473,10 @@ void analyzediff(void){
 			calculatedChecksum += receivebuffer[i];
 		}
 		
-		for (int z = 0; z < NR_OF_SAMPLES-4; z++){
+		for (int z = 0; z < NR_OF_SAMPLES-4; z++) {
 			symbol = receivebuffer[z]; // Generiert das Signal entsprechend der Zeit seit dem letzten "Peak"-Signal
 			// Decodiere das Symbol
-			switch (symbol){
+			switch (symbol) {
 				case 0:
 				checksumGL += receivebuffer[z];
 				break;
@@ -476,8 +499,7 @@ void analyzediff(void){
 			
 		}
 		
-		for (int i = 0; i < 32; i++)
-		{
+		for (int i = 0; i < 32; i++) {
 			receivebuffer[i] = 0; //Mutex!
 		}
 		checksumGL = 0;
@@ -485,11 +507,9 @@ void analyzediff(void){
 		break;
 		case 4:
 		
-		if (!((receivebuffer[0] == 0) && (receivebuffer[1] == 3) && (receivebuffer[2] == 0) && (receivebuffer[3] == 3))) //Gesammte Päckchen Anzahl muss durch 4 Sauber geteitl werden können
-		{
+		if (!((receivebuffer[0] == 0) && (receivebuffer[1] == 3) && (receivebuffer[2] == 0) && (receivebuffer[3] == 3))) {//Gesammte Päckchen Anzahl muss durch 4 Sauber geteitl werden können
 			k = 0;
-			for (int i = 0; i < 4; i++)
-			{
+			for (int i = 0; i < 4; i++) {
 				receivebuffer[i] = 0;
 			}
 		}
@@ -499,23 +519,17 @@ void analyzediff(void){
 	
 }
 
-void fillDecoderQueue(uint16_t buffer[NR_OF_SAMPLES])
-{
+void fillDecoderQueue(uint16_t buffer[NR_OF_SAMPLES]) {
 	BaseType_t xTaskWokenByReceive = pdFALSE;
-
 	xQueueSendFromISR( decoderQueue, &buffer[0], &xTaskWokenByReceive );
 }
 
-ISR(DMA_CH2_vect)
-{
+ISR(DMA_CH2_vect) {
 	DMA.CH2.CTRLB|=0x10;
-
 	fillDecoderQueue( &adcBuffer0[0] );
 }
 
-ISR(DMA_CH3_vect)
-{
+ISR(DMA_CH3_vect) {
 	DMA.CH3.CTRLB |= 0x10;
-
 	fillDecoderQueue( &adcBuffer1[0] );
 }
