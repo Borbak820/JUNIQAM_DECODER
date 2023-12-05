b295e7a0dfbcfe066d9a1161ae0f5fa52c08de49
Borbak820 <erikhaubrich@gmail.com>
2023-12-05 16:13:09 +0100

    Included Display-Task

---
+++ QAMDecGen/Display.c
@@ -0,0 +1,38 @@
+#include "avr_compiler.h"
+#include "pmic_driver.h"
+#include "TC_driver.h"
+#include "clksys_driver.h"
+#include "sleepConfig.h"
+
+#include "port_driver.h"
+#include "math.h"
+#include "string.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "queue.h"
+#include "event_groups.h"
+#include "stack_macros.h"
+#include "mem_check.h"
+#include "init.h"
+
+#include "utils.h"
+#include "errorHandler.h"
+#include "NHD0420Driver.h"
+#include "stdio.h"
+#include "Display.h"
+#include "qamdec.h"
+
+
+
+void vDisplay(void* pvParameters){
+	
+	resetReason_t reason = getResetReason();
+	vDisplayClear();
+	vDisplayWriteStringAtPos(0,0, "%f", reconstructedFloat);
+	vDisplayWriteStringAtPos(1,0, "%f", reconstructedFloat);
+	vDisplayWriteStringAtPos(2,0, "%f", reconstructedFloat);
+	vDisplayWriteStringAtPos(3,0,"ResetReason: %d", reason);
+	printf("Wert der globalen float-Variable in file1.c: %f\n", reconstructedFloat);
+	vTaskDelay(1000/portTICK_RATE_MS);
+}
---
+++ QAMDecGen/includes/Display.h
@@ -0,0 +1,15 @@
+/*
+ * Display.h
+ *
+ * Created: 05.05.2020 16:38:15
+ *  Author: Chaos
+ */ 
+
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+void vDisplay(void* pvParameters);
+extern float reconstructedFloat;
+
+#endif /*DISPLAY_H_ */
