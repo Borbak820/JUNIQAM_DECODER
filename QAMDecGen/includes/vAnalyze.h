eadbd28608ec4e26a7e4b261d38599c35d51fc3a
Borbak820 <erikhaubrich@gmail.com>
2024-01-09 22:00:45 +0100

    Renamed vTest to vAnalayze

---
+++ QAMDecGen/includes/vAnalyze.h
@@ -0,0 +1,16 @@
+/*
+ * vAnalyze.h
+ *
+ * Created: 05.12.2023 16:47:25
+ *  Author: mikaj
+ */ 
+
+
+#ifndef VANALYZE_H_
+#define VANALYZE_H_
+
+void vAnalyze(void *pvParameters);
+void vDisplay(void* pvParameters);
+
+
+#endif /* VANALYZE_H_ */
